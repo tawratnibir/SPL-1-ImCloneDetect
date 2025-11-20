7c1de1b3b506f94a04f7050ae0730739782986b4
tawratnibir <tawratnibir@gmail.com>
2025-11-20 15:47:40 +0600

    grayscale practice code written

--- practice/1.cpp
+++ practice/1.cpp
@@ -230,4 +230,31 @@ int main()
 {
     BMP bmp("tester.bmp");
     bmp.read("tester.bmp");
+
+    int height = bmp.bmpInfoHeader.height;
+    int width = bmp.bmpInfoHeader.width;
+    int bpp = bmp.bmpInfoHeader.bitCount / 8;
+    int rowStride = bpp * width;
+    vector<uint8_t> pixels = bmp.data;
+
+    for(int y=0;y<height;++y) {
+        for(int x=0; x< width; ++x) {
+            int idx = y * rowStride + x * bpp;
+
+            uint8_t B = pixels[idx + 0];
+            uint8_t G = pixels[idx + 1];
+            uint8_t R = pixels[idx + 2];
+
+            uint8_t Y = 0.299 * R + 0.587 * G + 0.114 * B;
+
+
+            pixels[idx + 0] = Y;
+            pixels[idx + 1] = Y;
+            pixels[idx + 2] = Y;
+        }
+    }
+
+    bmp.data = pixels;
+
+    bmp.write("less.bmp");
 }
