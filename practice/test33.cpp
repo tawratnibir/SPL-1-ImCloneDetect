bf035a583ab64bf88f68dde8b1b85d36fee573f4
tawratnibir <tawratnibir@gmail.com>
2025-11-21 16:31:21 +0600

    gaussian kernel creator added

--- practice/test33.cpp
+++ practice/test33.cpp
@@ -5,6 +5,10 @@
 
 int main() {
     BMP image1("tester.bmp");
-    toGrayScale(image1, "test33_gray.bmp");
-    toNegative(image1, "test33_neg.bmp");
+    std::vector<uint8_t> negativePixels, grayPixels, normalPixels;
+    grayPixels = toGrayScale(image1, "test33_gray.bmp");
+    negativePixels = toNegative(image1, "test33_neg.bmp");
+    BMP negativeImage("test33_neg.bmp");
+    negativePixels = pixelGenerator(negativeImage);
+    normalPixels = pixelGenerator(image1);
 }
